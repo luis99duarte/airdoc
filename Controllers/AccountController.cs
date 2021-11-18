b1310fe5c1147d0b92fa0ae9edb758943719b59b
Luis Duarte <luis99freitas@gmail.com>
2021-11-18 16:51:13 +0000

    AccountController.cs (line 175) continue redirect page on email not verified

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -172,7 +172,7 @@ namespace identity.Controllers
 
                         /*else
                         {
-                          
+                          return <view for email verification>
                         }
                         */
                     }
